792e01f5a289003d4f6a4fb5d6db7f6c26523e04
William Fish <william.michael.fish@gmail.com>
2025-03-10 15:35:08 +0000

    BAU: Configure asdf and nix dependencies
    Also configure pre-commit hooks to be closer to our other projects

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-ruby = {
+      url = "github:bobvanderlinden/nixpkgs-ruby";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-ruby }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+          overlays = [nixpkgs-ruby.overlays.default];
+        };
+
+        rubyVersion = builtins.head (builtins.split "\n" (builtins.readFile ./.ruby-version));
+        ruby = pkgs."ruby-${rubyVersion}";
+
+        lint = pkgs.writeScriptBin "lint" ''
+          changed_files=$(git diff --name-only --diff-filter=ACM --merge-base main)
+
+          bundle exec rubocop --autocorrect-all --force-exclusion $changed_files Gemfile
+        '';
+
+        psychBuildFlags = with pkgs; [
+          "--with-libyaml-include=${libyaml.dev}/include"
+          "--with-libyaml-lib=${libyaml.out}/lib"
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            export GEM_HOME=$PWD/.nix/ruby/$(${ruby}/bin/ruby -e "puts RUBY_VERSION")
+            mkdir -p $GEM_HOME
+
+            export GEM_PATH=$GEM_HOME
+            export PATH=$GEM_HOME/bin:$PATH
+            export BUNDLE_BUILD__PSYCH="${
+              builtins.concatStringsSep " " psychBuildFlags
+            }"
+          '';
+
+          buildInputs = [
+            lint
+            pkgs.circleci-cli
+            pkgs.graphviz
+            pkgs.nodejs_latest
+            ruby
+          ];
+        };
+      });
+}
